3d1f437b11d8adc9d44553c7a2a32e461f2305ff
Entyrious <yayanmadarauchih26@gmail.com>
2022-11-03 00:46:59 +0100

    yugioh project became genshin, made 2 js for cleaner purpose.

---
+++ GI ver/scripts/data.js
@@ -0,0 +1,71 @@
+var locations=[
+    "mondstadt",
+    "liyue"
+]
+var elements=[
+    "hydro",
+    "pyro",
+    "cryo",
+    "anemo",
+    "geo",
+    "electro"
+]
+var cards=[
+    {
+        name: "amber",
+        element: elements[1],
+        location: locations[0],
+        img: "./imgs/mon/amber.jpg",
+        atk: (Math.floor(Math.random()*100)+100)*10
+    },
+    {
+        name: "kaeya",
+        element: elements[2],
+        location: locations[0],
+        img: "./imgs/mon/kaeya.jpg",
+        atk: (Math.floor(Math.random()*100)+100)*10
+    },
+    {
+        name: "jean",
+        element: elements[3],
+        location: locations[0],
+        img: "./imgs/mon/jean.jpg",
+        atk: (Math.floor(Math.random()*100)+100)*10
+    },
+    {
+        name: "diluc",
+        element: elements[1],
+        location: locations[0],
+        img: "./imgs/mon/diluc.png",
+        atk: (Math.floor(Math.random()*100)+100)*10
+    },
+    {
+        name: "qiqi",
+        element: elements[2],
+        location: locations[1],
+        img: "./imgs/liy/qiqi.jpg",
+        atk: (Math.floor(Math.random()*100)+100)*10
+    },
+    {
+        name: "keqing",
+        element: elements[5],
+        location: locations[1],
+        img: "./imgs/liy/keqing.jpg",
+        atk: (Math.floor(Math.random()*100)+100)*10
+    },
+    {
+        name: "xingqiu",
+        element: elements[0],
+        location: locations[1],
+        img: "./imgs/liy/xingqiu.jpg",
+        atk: (Math.floor(Math.random()*100)+100)*10
+    },
+    {
+        name: "yun jin",
+        element: elements[4],
+        location: locations[1],
+        img: "./imgs/liy/yun_jin.jpg",
+        atk: (Math.floor(Math.random()*100)+100)*10
+    }
+
+]
---
+++ GI ver/scripts/script.js
@@ -0,0 +1,37 @@
+var m1_zone = document.getElementById("monsterzone1")
+var m2_zone = document.getElementById("monsterzone2")
+
+for (let i = 0; i < 3; i++) {
+    var randomnum=Math.floor(Math.random()*cards.length)
+    var newCard = document.createElement("div")
+    newCard.addEventListener("click", flipping)
+    newCard.setAttribute("class","cards flip")
+    newCard.style.background="url("+cards[randomnum].img+")"
+    
+    var atkv=document.createElement("h2")
+    atkv.textContent=cards[randomnum].atk
+    atkv.setAttribute("class","atkv "+cards[randomnum].element)
+    newCard.append(atkv)
+
+    m1_zone.append(newCard)
+}
+
+for (let i = 0; i < 3; i++) {
+    var randomnum=Math.floor(Math.random()*cards.length)
+    var newCard = document.createElement("div")
+    newCard.addEventListener("click", flipping)
+    newCard.setAttribute("class","cards")
+    newCard.style.background="url("+cards[randomnum].img+")"
+    
+    var atkv=document.createElement("h2")
+    atkv.textContent=cards[randomnum].atk
+    atkv.setAttribute("class","atkv "+cards[randomnum].element)
+    newCard.append(atkv)
+
+    m2_zone.append(newCard)
+}
+
+function flipping(event) {
+        console.log(event)
+        event.currentTarget.classList.toggle("back")
+}
