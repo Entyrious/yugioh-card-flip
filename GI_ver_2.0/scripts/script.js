9f2edcf9956be7955eba4a94dd42f31afcbc9c28
Entyrious <yayanmadarauchih26@gmail.com>
2022-11-09 02:06:50 +0100

    made an array to make the card game look better by adding space in the middle and the selected card goes bigger but only consuming the space in between the two players.

--- GI_ver_2.0/scripts/script.js
+++ GI_ver_2.0/scripts/script.js
@@ -2,6 +2,8 @@ var m1_zone = document.getElementById("monsterzone1")
 var m2_zone = document.getElementById("monsterzone2")
 var h1_zone = document.getElementById("handcards1")
 var h2_zone = document.getElementById("handcards2")
+var charElements1 = []
+var charElements2 = []
 
 function generateCards(zone, count, set, isFlip){
     for (let i = 0; i < count; i++) {
@@ -22,12 +24,31 @@ function generateCards(zone, count, set, isFlip){
         newCard.append(atkv)
     
         zone.append(newCard)
+        if(count == 4){
+            if(isFlip == true){
+                charElements1.push(newCard)
+            }
+            else{
+                charElements2.push(newCard)
+            }
+        }
     };
 };
 
 function flipping(event) {
+    if(event.currentTarget.classList.contains("flip")){
+        for (let i = 0; i< charElements1.length; i=i+1){
+           charElements1[i].classList.remove("on-field")
+        }
+    }
+    else{
+        for (let i = 0; i< charElements2.length; i=i+1){
+           charElements2[i].classList.remove("on-field")
+        }
+    }
+
         console.log(event)
-        event.currentTarget.classList.toggle("back")
+        event.currentTarget.classList.toggle("on-field")
 };
 
 generateCards(h1_zone, 5, cards, true)
